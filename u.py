cbbcc2d5c293498bff51f707bcdcedfd436a4b83
Cracker <85168111+cracker911181@users.noreply.github.com>
2021-09-12 03:06:32 +0600

    Update u.py

--- u.py
+++ u.py
@@ -4,8 +4,8 @@ os.system("rm -rf $HOME/Cracker-Tool")
 
 os.system("git clone https://github.com/cracker911181/Cracker-Tool")
 
-os.system("cd Cracker-Tool")
+os.system("cd $HOME/Cracker-Tool")
 try:
-	os.system("python cracker-main.py")
+	os.system("python $HOME/Cracker-Tool/cracker-main.py")
 except:
 	print()
