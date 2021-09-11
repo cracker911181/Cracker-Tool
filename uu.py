ecee83bc97342ccd695544bf4571ceb6c8190aba
Cracker <85168111+cracker911181@users.noreply.github.com>
2021-09-12 03:10:51 +0600

    Update uu.py

--- uu.py
+++ uu.py
@@ -4,8 +4,5 @@ os.system("rm -rf $HOME/Cracker-Tool")
 
 os.system("git clone https://github.com/cracker911181/Cracker-Tool")
 
-os.system("cd $HOME/Cracker-Tool")
-try:
-	os.system("python $HOME/Cracker-Tool/cracker-main.py")
-except:
-	print()
+os.system("cd Cracker-Tool")
+
