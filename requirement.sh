521f95a35b28f5852344874150f8ac2f27e37bad
Cracker <85168111+cracker911181@users.noreply.github.com>
2021-10-08 00:56:14 +0600

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -5,6 +5,7 @@ pip install pyfiglet
 pip install bs4
 pip install lolcat
 pip install git+https://github.com/cracker911181/cracker_say
+pip install git+https://github.com/cracker911181/cracker_enc
 pip install cowsay 
 pip install youtube_dl
 pip install gTTS
