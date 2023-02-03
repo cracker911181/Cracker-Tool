4839e660193363855ec2932b21f05bc4e7e47a93
Abdur Rabbi <yeasin4905@gmail.com>
2023-02-04 04:44:50 +0600

    Update requirement.sh

--- requirement.sh
+++ requirement.sh
@@ -9,7 +9,8 @@ pip install pyfiglet
 pip install bs4
 pip install lolcat
 #pip install -i https://test.pypi.org/simple/ cracker-say
-pip install git+https://github.com/cracker911181/cracker_say
+#pip install git+https://github.com/cracker911181/cracker_say
+pip install cracker-saying
 pip install cowsay 
 pip install youtube_dl
 pip install gTTS
